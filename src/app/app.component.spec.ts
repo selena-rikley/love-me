b211e589d88eab0e8d87b1733f2e55e916504d98
kerianne <kerianne.rikley@gmail.com>
2018-06-15 22:59:16 -0400

    added start button

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,16 +17,35 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'love-me'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('love-me');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
+    expect(compiled.querySelector('h1').textContent).toContain('love-me');
   }));
+
+  it('should render start button', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button').textContent).toContain('start');
+  }));
+
+  it('should start game when start button is clicked', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    const spy = spyOn(fixture.componentInstance, 'startGame');
+    const button = compiled.querySelector('button');
+    button.click();
+    fixture.whenStable().then(() => {
+      expect(spy).toHaveBeenCalled();
+    });
+  }));
+
 });
