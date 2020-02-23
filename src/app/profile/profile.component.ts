21cca4fa3b305a803de6051742c5af347f8ce34c
kerianne-rikley <kerianne.rikley@gmail.com>
2020-02-23 17:10:46 -0500

    Added player appearance selection to profile and appearance to character

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Character, CharacterTag, pronouns } from '../character/character';
-import { NgForm } from '@angular/forms'
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -9,7 +9,7 @@ import { NgForm } from '@angular/forms'
 })
 export class ProfileComponent implements OnInit {
 
-  user = new Character('name', CharacterTag.Player, 'N');
+  user = new Character('name', CharacterTag.Player, 'N', 1);
 
   constructor() { }
 
@@ -19,8 +19,8 @@ export class ProfileComponent implements OnInit {
   goBack() { }
 
   submit(form: NgForm) {
-    this.user = new Character(form.controls.playerName.value, CharacterTag.Player, form.controls.playerPronouns.value);
+    this.user = new Character(form.controls.playerName.value, CharacterTag.Player, form.controls.playerPronouns.value, form.controls.playerAppearance.value);
     // Added for testing purposes. May change later.
-    alert(form.controls.playerPronouns.value + form.controls.playerName.value);
+    alert(form.controls.playerPronouns.value + form.controls.playerName.value + form.controls.playerAppearance.value);
   }
 }
